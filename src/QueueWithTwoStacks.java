a0b68771d56dbc4afe237632d9c90ac4d0018a25
Alex Lee <phparkle@gmail.com>
2017-09-01 21:46:11 +0800

    Add comments

--- src/QueueWithTwoStacks.java
+++ src/QueueWithTwoStacks.java
@@ -2,39 +2,48 @@ import edu.princeton.cs.algs4.Stack;
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 
+// A queue implemented with two stacks.
 public class QueueWithTwoStacks<Item> {
 
-    private Stack<Item> in = new Stack<>();
-    private Stack<Item> out = new Stack<>();
+	// The two stacks.
+    private Stack<Item> stack1 = new Stack<>();
+    private Stack<Item> stack2 = new Stack<>();
 
+    // Returns true if the queue is empty.
     public boolean isEmpty() {
-        return in.isEmpty() && out.isEmpty();
+        return stack1.isEmpty() && stack2.isEmpty();
     }
 
+    // Returns the number of items in the queue.
     public int size() {
-        return in.size() + out.size();
+        return stack1.size() + stack2.size();
     }
 
+    // Returns the first item in the queue.
     public Item peek() {
         reverse();
-        return out.peek();
+        return stack2.peek();
     }
 
+    // Adds an item to the queue.
     public void enqueue(Item item) {
-        in.push(item);
+        stack1.push(item);
     }
 
+    // Removes and returns the first item in the queue.
     public Item dequeue() {
         reverse();
-        return out.pop();
+        return stack2.pop();
     }
 
+    // If stack2 is empty, moves all items from stack1 to stack2.
     private void reverse() {
-        if (out.isEmpty())
-            while (!in.isEmpty())
-                out.push(in.pop());
+        if (stack2.isEmpty())
+            while (!stack1.isEmpty())
+                stack2.push(stack1.pop());
     }
 
+    // Tests the class.
     public static void main(String[] args) {
         QueueWithTwoStacks<String> queue = new QueueWithTwoStacks<String>();
         while (!StdIn.isEmpty()) {
--- src/StackWithMax.java
+++ src/StackWithMax.java
@@ -2,43 +2,55 @@ import edu.princeton.cs.algs4.Stack;
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 
+// A stack with return-the-maximum operation.
 public class StackWithMax {
 
+	// The stack for the items.
     private Stack<Integer> stack = new Stack<>();
+
+    // The stack for the maximum items.
     private Stack<Integer> max = new Stack<>();
 
+    // Returns true if the stack is empty.
     public boolean isEmpty() {
         return stack.isEmpty();
     }
 
+    // Returns the number of items in the stack.
     public int size() {
         return stack.size();
     }
 
+    // Adds an item to the stack.
     public void push(int item) {
         stack.push(item);
         if (max.isEmpty() || item >= max.peek())
         	max.push(item);
     }
 
+    // Removes and returns the last item on the stack.
     public int pop() {
         int item = stack.pop();
         if (max.peek() == item) max.pop();
         return item;
     }
 
+    // Returns the last item on the stack.
     public int peek() {
         return stack.peek();
     }
 
+    // Returns the maximum item in the stack.
     public int max() {
         return max.peek();
     }
 
+    // Returns a string representation of the stack.
     public String toString() {
         return stack.toString();
     }
 
+    // Tests the class.
     public static void main(String[] args) {
         StackWithMax stack = new StackWithMax();
         while (!StdIn.isEmpty()) {
